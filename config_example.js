03700804f0878133bd396373be00e79a3a7c9777
Takeshi <takeshi.gi75@gmail.com>
2026-04-07 12:03:40 +0700

    feat: add adminGuildId configuration and bump version to 6.0-alpha

--- config_example.js
+++ config_example.js
@@ -2,6 +2,7 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
+	adminGuildId: "ServerID", //- ID Server trụ sở chính của Admin (Lệnh Admin chỉ hiện ở đây)
 	token: process.env.token || "YOUR_TOKEN_HERE", //- Bot's Token
 	clientId: process.env.clientId || "YOUR_CLIENT_ID", //- ID of the bot
 	clientSecret: process.env.clientSecret || "YOUR_CLIENT_SECRET", //- Client Secret of the bot
